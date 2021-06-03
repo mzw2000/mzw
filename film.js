e7f35e07203039ca0207c61e3373b91b23016a93
mzw2000 <68105672+mzw2000@users.noreply.github.com>
2021-06-03 21:18:45 +0800

    Delete film.js

--- film.js
+++
@@ -1,4 +0,0 @@
-[{"id":1,"name":"死侍", "picurl":"https://img2.askpanda.cc/ask/movie/6378/6378-dm212016052303600_wu400.jpg", "date":"2012-02-16" },
-{"id":2,"name":"肖申克的救赎", "picurl":"https://img2.askpanda.cc/ask/movie/9471/9471-dm820160526224407_wu400.jpg","date":"1994-09-22"},
-{"id":3,"name":"玩具总动员3", "picurl":"https://img2.askpanda.cc/ask/movie/9627/9627-dm12016052763518_wu400.jpg", "date":"2010-06-18" },
-{"id":4, "name":"这个杀手不太冷","picurl":"https://img2.askpanda.cc/ask/movie/9472/9472-dm320160526224827_wu400.jpg","date":"1994-11-18"},]
